cc4c25ffdb6142ec2c3e237bcd3d8c1e97e8866c
stop-pattern <railwaysandstationfan+Git@gmail.com>
2020-08-28 04:36:40 +0900

    [add] プロジェクト
    仮にTISで空のプロジェクトを作成
    すぐ使えるようにそれっぽい実装を移植

---
+++ caMon.pages.TIS/TIS.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+using TR;
+
+namespace caMon.pages.TIS
+{
+    /// <summary>
+    /// TIS.xaml の相互作用ロジック
+    /// </summary>
+    public partial class TIS : Page
+	{
+		caMonIF camonIF;
+		public TIS(caMonIF arg_camonIF)
+		{
+			InitializeComponent();
+
+			camonIF = arg_camonIF;
+		}
+
+		private void NextP(object sender, RoutedEventArgs e) => camonIF.BackToHomeDo();
+	}
+}
---
+++ caMon.pages.TIS/caMonIF.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace caMon.pages.TIS
+{
+	public class caMonIF : IPages
+	{
+		public Page FrontPage => new TIS(this);
+
+		public event EventHandler BackToHome;
+		public event EventHandler CloseApp;
+
+		public caMonIF()
+		{
+
+		}
+
+		public void Dispose()
+		{
+			//throw new NotImplementedException();
+		}
+
+		internal void BackToHomeDo() => BackToHome?.Invoke(null, null);
+	}
+}
