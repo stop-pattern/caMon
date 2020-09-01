d27c84e3cdf040acd2a9192e069f5b0ae781cfc0
stop-pattern <railwaysandstationfan+Git@gmail.com>
2020-09-02 02:36:47 +0900

    [add] ボタン・各関数追加
    戻る/終了ボタン追加
    必要関数群追加

--- caMon.pages.TIS/Root.xaml.cs
+++ caMon.pages.TIS/Root.xaml.cs
@@ -11,6 +11,11 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.Windows.Threading;
+
+using TR;
+using TR.BIDSSMemLib;
+
 namespace caMon.pages.TIS
 {
     /// <summary>
@@ -18,9 +23,108 @@ namespace caMon.pages.TIS
     /// </summary>
     public partial class Root : Page
     {
-        public Root()
+        /// <summary>ループタイマー</summary>
+        DispatcherTimer timer = new DispatcherTimer();
+        /// <summary>  </summary>
+        caMonIF camonIF;
+        /// <summary>ループ間隔[ms]</summary>
+        int timerInterval = 300;
+        /// <summary>BIDS Shared Memoryの状態</summary>
+        bool BIDSSMemIsEnabled = false;
+        /// <summary>Bve5から渡される情報</summary>
+        BIDSSharedMemoryData bve5;
+        /// <summary>OpenBveから渡される情報</summary>
+        OpenD obve;
+        /// <summary>panelの状態</summary>
+        int[] panel;
+        /// <summary>soundの状態</summary>
+        int[] sound;
+
+
+        public Root(caMonIF arg_camonIF)
         {
             InitializeComponent();
+
+            camonIF = arg_camonIF;
+
+            SharedFuncs.SML.SMC_BSMDChanged += SMemLib_BIDSSMemChanged;
+            SharedFuncs.SML.SMC_OpenDChanged += SMemLib_OpenChanged;
+            SharedFuncs.SML.SMC_PanelDChanged += SMemLib_PanelChanged;
+            SharedFuncs.SML.SMC_SoundDChanged += SMemLib_SoundChanged;
+
+            panel = new int[256];
+            panel = new int[256];
+
+            timer.Tick += Timer_Tick;
+            timer.Interval = new TimeSpan(0, 0, 0, 0, timerInterval);
+            timer.Start();
+        }
+
+        /// <summary>
+        /// タイマー更新
+        /// </summary>
+        private void TimerStart()
+        {
+            timer.Stop();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, timerInterval);
+            timer.Start();
+        }
+
+        /// <summary> 
+        /// mod選択画面へ戻る
+        /// </summary>
+        private void back_home(object sender, RoutedEventArgs e) => camonIF.BackToHomeDo();
+
+        /// <summary> 
+        /// アプリケーションを終了
+        /// </summary>
+        private void close_app(object sender, RoutedEventArgs e) => camonIF.CloseAppDo();
+
+        /// <summary> 
+        /// mainFrameにPage.xamlの内容を描画
+        /// </summary>
+        private void Page__Show(object sender, RoutedEventArgs e) => mainFrame.Source = new Uri(@"Pages\Page.xaml", UriKind.Relative);
+
+
+        /// <summary> 
+        /// SharedMemに更新があったときに呼ばれる関数
+        /// </summary>
+        private void SMemLib_BIDSSMemChanged(object sender, ValueChangedEventArgs<BIDSSharedMemoryData> e)
+        {
+            BIDSSMemIsEnabled = e.NewValue.IsEnabled;
+            bve5 = e.NewValue;
+        }
+
+        /// <summary> 
+        /// Openに更新があったときに呼ばれる関数
+        /// </summary>
+        private void SMemLib_OpenChanged(object sender, ValueChangedEventArgs<OpenD> e)
+        {
+            obve = e.NewValue;
+        }
+
+        /// <summary> 
+        /// Panelに更新があったときに呼ばれる関数
+        /// </summary>
+        private void SMemLib_PanelChanged(object sender, ValueChangedEventArgs<int[]> p)
+        {
+            panel = p.NewValue;
+        }
+
+        /// <summary> 
+        /// Soundに更新があったときに呼ばれる関数
+        /// </summary>
+        private void SMemLib_SoundChanged(object sender, ValueChangedEventArgs<int[]> s)
+        {
+            sound = s.NewValue;
+        }
+
+
+        /// <summary> 
+        /// タイマで呼ばれる関数
+        /// </summary>
+        private void Timer_Tick(object sender, object e)
+        {
         }
     }
 }
--- caMon.pages.TIS/caMonIF.cs
+++ caMon.pages.TIS/caMonIF.cs
@@ -5,23 +5,24 @@ using System.Windows.Controls;
 
 namespace caMon.pages.TIS
 {
-	public class caMonIF : IPages
-	{
-		public Page FrontPage => new TIS(this);
+    public class caMonIF : IPages
+    {
+        public Page FrontPage => new Root(this);
 
-		public event EventHandler BackToHome;
-		public event EventHandler CloseApp;
+        public event EventHandler BackToHome;
+        public event EventHandler CloseApp;
 
-		public caMonIF()
-		{
+        public caMonIF()
+        {
 
-		}
+        }
 
-		public void Dispose()
-		{
-			//throw new NotImplementedException();
-		}
+        public void Dispose()
+        {
+            //throw new NotImplementedException();
+        }
 
-		internal void BackToHomeDo() => BackToHome?.Invoke(null, null);
-	}
+        internal void BackToHomeDo() => BackToHome?.Invoke(null, null);
+        internal void CloseAppDo() => CloseApp?.Invoke(null, null);
+    }
 }
