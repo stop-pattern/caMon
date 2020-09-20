1d8fee016cc8c00dfc686e20155a2e0f0d275822
stop-pattern <railwaysandstationfan+Git@gmail.com>
2020-09-21 00:30:37 +0900

    [add] 定速・回生・接続状況

--- caMon.pages.TIS/pages/Driving.xaml.cs
+++ caMon.pages.TIS/pages/Driving.xaml.cs
@@ -1,15 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
+
+using TR;
+using TR.BIDSSMemLib;
 
 namespace caMon.pages.TIS.pages
 {
@@ -18,9 +13,114 @@ namespace caMon.pages.TIS.pages
     /// </summary>
     public partial class Driving : Page
     {
-        public Driving()
+        /// <summary> ループタイマー </summary>
+        DispatcherTimer timer = new DispatcherTimer();
+        int timerInterval = 300;
+        bool BIDSSMemIsEnabled = false;
+        int brakeNotch;
+        int powerNotch;
+        int reverserPosition;
+        bool constantSpeed;
+        bool door = false;
+        int[] panel;
+        int[] sound;
+
+        public Driving(caMonIF arg_camonIF)
         {
             InitializeComponent();
+
+            SharedFuncs.SML.SMC_BSMDChanged += SMemLib_BIDSSMemChanged;
+            SharedFuncs.SML.SMC_OpenDChanged += SMemLib_OpenChanged;
+            SharedFuncs.SML.SMC_PanelDChanged += SMemLib_PanelChanged;
+            SharedFuncs.SML.SMC_SoundDChanged += SMemLib_SoundChanged;
+
+            panel = new int[256];
+            sound = new int[256];
+        }
+
+        /// <summary> 
+        /// SharedMemに更新があったときに呼ばれる関数
+        /// </summary>
+        private void SMemLib_BIDSSMemChanged(object sender, ValueChangedEventArgs<BIDSSharedMemoryData> e)
+        {
+            BIDSSMemIsEnabled = e.NewValue.IsEnabled;
+
+            brakeNotch = e.NewValue.HandleData.B;
+            powerNotch = e.NewValue.HandleData.P;
+            reverserPosition = e.NewValue.HandleData.R;
+            switch (e.NewValue.HandleData.C)
+            {
+                case 1:
+                    constantSpeed = true;
+                    break;
+                case 2:
+                    constantSpeed = false;
+                    break;
+                case 0:
+                default:
+                    break;
+            }
+
+            door = e.NewValue.IsDoorClosed;
+        }
+
+        /// <summary> 
+        /// Openに更新があったときに呼ばれる関数
+        /// </summary>
+        private void SMemLib_OpenChanged(object sender, ValueChangedEventArgs<OpenD> e)
+        {
+        }
+
+        /// <summary> 
+        /// Panelに更新があったときに呼ばれる関数
+        /// </summary>
+        private void SMemLib_PanelChanged(object sender, ValueChangedEventArgs<int[]> p)
+        {
+            panel = p.NewValue;
+        }
+
+        /// <summary> 
+        /// Soundに更新があったときに呼ばれる関数
+        /// </summary>
+        private void SMemLib_SoundChanged(object sender, ValueChangedEventArgs<int[]> s)
+        {
+            sound = s.NewValue;
+        }
+
+        /// <summary> 
+        /// タイマで呼ばれる関数
+        /// </summary>
+        private void Timer_Tick(object sender, object e)
+        {
+            /// 接続
+            if (BIDSSMemIsEnabled)
+            {
+                Online.Visibility = Visibility.Visible;
+                Offline.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                Online.Visibility = Visibility.Collapsed;
+                Offline.Visibility = Visibility.Visible;
+            }
+
+            /// 回生
+            if (panel[52] != 0) Regeneration.Visibility = Visibility.Visible;
+            else Regeneration.Visibility = Visibility.Collapsed;
+
+            /// 定速
+            if (constantSpeed) ConstantSpeed.Visibility = Visibility.Visible;
+            else ConstantSpeed.Visibility = Visibility.Collapsed;
+        }
+
+        /// <summary>
+        /// タイマー更新
+        /// </summary>
+        private void TimerStart()
+        {
+            timer.Stop();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, timerInterval);
+            timer.Start();
         }
     }
 }
